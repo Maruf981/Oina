85e14e89fa1fe7697d281fb7aaf6181ae1ec26fd
Maruf981 <marufabdu708@gmail.com~>
2026-09-08 13:09:53 +0300

    Fix missing CallbackQuery import causing bot_admin crash loop

--- bot_admin/bot.py
+++ bot_admin/bot.py
@@ -6,7 +6,7 @@ from aiogram.filters import Command
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import State, StatesGroup
 from aiogram.fsm.storage.memory import MemoryStorage
-from aiogram.types import Message, ReplyKeyboardMarkup, KeyboardButton, WebAppInfo, InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram.types import Message, ReplyKeyboardMarkup, KeyboardButton, WebAppInfo, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 from dotenv import load_dotenv
 import httpx
 
